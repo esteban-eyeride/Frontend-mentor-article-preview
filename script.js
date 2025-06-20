c0bb75e8b247fe846089fc990093c2d32717ecf0
esteban-eyeride <esteban.c@eyeride.io>
2025-06-20 16:42:21 -0500

    Add files via upload
    updated html, css and JS to better comply with frontend mentor improved solution

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const shareBtn = document.getElementById("shareBtn");
 const tooltip = document.getElementById("tooltip");
-const body = document.querySelector("body");
 const tooltip2 = document.getElementById("tooltip2");
-const mQuery = window.matchMedia("max-width: 768px");
 
 shareBtn.addEventListener("click", () => {
     tooltip2.classList.toggle("tooltipMove")
